aef309bc326d5296d84f39c4dc28342db4c56b83
Mr_Shell <blueshell@whu.edu.cn>
2018-03-15 19:29:28 +0800

    Add request.py

---
+++ request.py
@@ -0,0 +1,19 @@
+import requests
+def recon():
+    """
+    重新连接网络
+    :return:
+    """
+    user_Agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36"
+    headers={'User-Agent':user_Agent}
+    data = {"DDDDD":"lcl",
+            "upass":"12345678",
+            "0MKKey":"登录 Login"}
+    try:
+        r = requests.get("https://www.baidu.com/",headers=headers,timeout=3)
+        print("Every thing is ok")
+    except:
+        r = requests.post("http://192.168.100.3/", data=data, headers=headers)
+        print(r.status_code)
+
+recon()
